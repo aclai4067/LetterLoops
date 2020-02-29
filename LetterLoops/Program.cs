50cea1ee32d84327f4baac8039ace1728e7e601c
Ashley Claiborne <languageisaliquid@gmail.com>
2020-02-29 10:31:44 -0600

    Add project files.

---
+++ LetterLoops/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Text;
+
+namespace LetterLoops
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a series of letters with no spaces:");
+            var userInput = Console.ReadLine();
+            var counter = 0;
+            StringBuilder build = new StringBuilder();
+
+            
+            foreach (var letter in userInput)
+            {
+                build.Append(letter.ToString().ToUpper());
+                if (counter > 0)
+                {
+                    for (int i = 0; i < counter; i++)
+                    {
+                        build.Append(letter.ToString().ToLower());
+                    }
+                }
+                counter += 1;
+
+                if (counter < userInput.Length)
+                {
+                    build.Append("-");
+                }
+            }
+
+            var finalString = build.ToString();
+
+            Console.WriteLine(finalString);
+            Console.ReadKey();
+        }
+    }
+}
